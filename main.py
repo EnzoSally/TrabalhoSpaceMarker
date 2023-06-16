7df441f399c5c4c4ab6efc6e5412d7a1f2fc0263
Enzo G. Almeida <myenzo121@gmail.com>
2023-06-16 04:57:57 -0300

    coloquei o Tkinter e fiz com que cada clique botasse uma caixa de dialogo pedindo nome da estrela e quando nao comentado ele escreve desconhecido + a coordenada na tela

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import tkinter as tk
+from tkinter import simpledialog
 
 pygame.init()
 
@@ -17,14 +19,29 @@ class Ball:
         self.y = y
         self.radius = 3.25
         self.bola_anterior = None
+        self.nome = ""
+
+    def definir_nome(self):
+        self.nome = simpledialog.askstring("Nome da Estrela", "Insira o nome da Estrela:")
 
     def draw(self):
         pygame.draw.circle(tela, vermelho, (self.x, self.y), self.radius)
         if self.bola_anterior:
             pygame.draw.line(tela, vermelho, (self.x, self.y), (self.bola_anterior.x, self.bola_anterior.y), 1)
 
-# Loop principal do jogo
+    def draw_name(self):
+        fonte = pygame.font.SysFont(None, 20)
+        if self.nome:
+            texto = fonte.render(self.nome, True, (0, 0, 0))
+        else:
+            texto = fonte.render(f"DESCONHECIDO ({self.x}, {self.y})", True, (0, 0, 0))
+        texto_rect = texto.get_rect(center=(self.x, self.y - self.radius - 10))
+        tela.blit(texto, texto_rect)
 
+root = tk.Tk()
+root.withdraw()
+
+# Loop principal do jogo
 running = True
 bola_anterior = None
 while running:
@@ -37,11 +54,18 @@ while running:
                 bola = Ball(x, y)
                 bola.bola_anterior = bola_anterior
                 bola_anterior = bola
+                bola.definir_nome()
                 bolas.append(bola)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                for bola in bolas:
+                    if not bola.nome:
+                        bola.nome = f"DESCONHECIDO ({bola.x}, {bola.y})"
 
     tela.fill(branco)
 
     for bola in bolas:
+        bola.draw_name()
         bola.draw()
 
     pygame.display.flip()
